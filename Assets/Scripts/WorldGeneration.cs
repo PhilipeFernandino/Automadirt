6b8b4d9380b0bafd249503f56845f3196c39836a
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2021-12-15 13:58:14 -0300

    Removido perlinGen e a geração com simplex é mais relacionada a altura

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -5,68 +5,36 @@ using UnityEngine.Tilemaps;
 
 public class WorldGeneration : MonoBehaviour
 {
-    public int mapHeight = 100, mapWidth = 1000, seed = 0;
+    public int mapHeight = 100, mapWidth = 1000, seed = 0, ugHeight;
 
-    public float basicTerrainScale = 20f, stoneHeight = 1;
-    public float ironScale = 20f, ironHeight = 1f;
-    public float simplexFrequency;
-    [Range(0, 1)] public float ironProbability;
+    public float simpFreq;
+    [Range(0, 1)] public float sfStonePb = 0.2f, ugDirtPb = 0.2f;
 
     public Tile dirt, stone, iron;
     public Tilemap tilemap;
-    public BoundsInt area;
-
-    void GenerateWithNoise() {
-        Random.InitState(seed);
-        int maxAxis = Mathf.Max(mapHeight, mapWidth);
-        
-        //Gerar o mapa básico        
-        {
-            float offsetX = Random.Range(-1000000, 1000000);
-            float offsetY = Random.Range(-1000000, 1000000);
-            
-            for (int w = 0; w < mapWidth; w++) {
-                for (int h = 0; h < mapHeight; h++) {
-                    if (PerlinNoise.GetPixel(w, h, maxAxis, maxAxis, basicTerrainScale, offsetX, offsetY) 
-                    > (float) h * stoneHeight / mapHeight) 
-                        tilemap.SetTile(new Vector3Int(w, h, 0), stone);
-                    else  
-                        tilemap.SetTile(new Vector3Int(w, h, 0), dirt);
-                }
-            }
-        }
-
-        //Adicionar ores
-        {
-            float offsetX = Random.Range(-1000000, 1000000);
-            float offsetY = Random.Range(-1000000, 1000000);
-            
-            for (int w = 0; w < mapWidth; w++) {               
-                for (int h = 0; h < mapHeight; h++) {
-                    if (PerlinNoise.GetPixel(w, h, maxAxis, maxAxis, ironScale, offsetX, offsetY) 
-                    > ((float) h / mapHeight * ironHeight + (1 - ironProbability))) 
-                        tilemap.SetTile(new Vector3Int(w, h, 0), iron);
-                }
-            }
-        }
-
-    }
 
     void GenerateWithSimplex() {
         FastNoiseLite noise = new FastNoiseLite();
         noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
         noise.SetSeed(seed);
-        noise.SetFrequency(simplexFrequency);
+        noise.SetFrequency(simpFreq);
+
         {
-            float offsetX = Random.Range(-1000000, 1000000);
-            float offsetY = Random.Range(-1000000, 1000000);
-            
             for (int w = 0; w < mapWidth; w++) {
                 for (int h = 0; h < mapHeight; h++) {
-                    if (noise.GetNoise(w, h) > (float) h * stoneHeight / mapHeight) 
-                        tilemap.SetTile(new Vector3Int(w, h, 0), stone);
-                    else  
-                        tilemap.SetTile(new Vector3Int(w, h, 0), dirt);
+                    if (h > ugHeight) {
+                        if ((Helper.TSimplexRange(noise.GetNoise(w, h))) > sfStonePb)
+                            tilemap.SetTile(new Vector3Int(w, h, 0), dirt);
+                        else  
+                            tilemap.SetTile(new Vector3Int(w, h, 0), stone);
+                    }
+                    else {
+                        if ((Helper.TSimplexRange(noise.GetNoise(w, h))) > ugDirtPb)
+                            tilemap.SetTile(new Vector3Int(w, h, 0), stone);
+                        else  
+                            tilemap.SetTile(new Vector3Int(w, h, 0), dirt);
+                    }
+
                 }
             }
         }
@@ -81,10 +49,6 @@ public class WorldGeneration : MonoBehaviour
     void OnGUI() {
         if(GUI.Button(new Rect(10,30,100,30), "Limpar")) {
             tilemap.ClearAllTiles();
-        }
-        if(GUI.Button(new Rect(10,70,100,30), "Perlin")) {
-            StartCoroutine(DebugTime());
-            GenerateWithNoise();
         }
         if(GUI.Button(new Rect(10,110,100,30), "Simplex")) {
             StartCoroutine(DebugTime());
