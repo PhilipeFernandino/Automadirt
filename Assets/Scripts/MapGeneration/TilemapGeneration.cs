5b89a2a67a02a5cc261d932c3bf9f62cbbdf1076
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2022-02-06 01:09:10 -0300

    Commitando pq crashou ;)

--- Assets/Scripts/MapGeneration/TilemapGeneration.cs
+++ Assets/Scripts/MapGeneration/TilemapGeneration.cs
@@ -19,7 +19,6 @@ public class TilemapGeneration : MonoBehaviour {
 
     public void GenerateFromTexture() {
         
-        Debug.Log("Gerando");
         int ctl = colorToTiles.Count;
         BoundsInt area = new BoundsInt();
         area.size = new Vector3Int(500, 500, 0);
@@ -31,13 +30,11 @@ public class TilemapGeneration : MonoBehaviour {
             for (int h = 0; h < mapHeight; h++) {
                 for (int c = 0; c < ctl; c++) {
                     if (colorToTiles[c].color == texture.GetPixel(w, h)) {
-                        Debug.Log("Setando tile");
-                        tilemap.SetTile(new Vector3Int(w, h, 1), colorToTiles[c].tile);
+                        tiles[w * mapWidth + h] = colorToTiles[c].tile;
                     }
-                        // tiles[w * mapWidth + h] = colorToTiles[c].tile;
                 }   
             }
         }
-        // tilemap.SetTilesBlock(area, tiles);
+        tilemap.SetTilesBlock(area, tiles);
     }
 }
